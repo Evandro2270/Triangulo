553bff22162fa259ae7809ba277a5923f2c54af9
Evandro2270 <evandro535@gmail.com>
2024-07-23 08:36:43 -0300

    Adicionar arquivos de projeto.

---
+++ Triangulo/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Globalization;
+namespace Programa
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            {
+
+                CultureInfo CI = CultureInfo.InvariantCulture;
+
+                double xA, xB, xC, yA, yB, yC;  
+
+                Console.WriteLine("Entre com as medidas do triangulo x:");
+                xA = double.Parse(Console.ReadLine(), CI);
+                xB = double.Parse(Console.ReadLine(), CI);
+                xC = double.Parse(Console.ReadLine(), CI);
+
+                Console.WriteLine("Entre com as medidas do triangulo x:");
+                yA = double.Parse(Console.ReadLine(), CI);
+                yB = double.Parse(Console.ReadLine(), CI);
+                yC = double.Parse(Console.ReadLine(), CI);
+
+                double p = (xA + xB + xC) / 2.0;
+                double areaX = Math.Sqrt(p * (p - xA) * (p - xB) * (p - xC));
+
+                p = (yA + yB + yC) / 2.0;
+                double areaY = Math.Sqrt(p * (p - yA) * (p - yB) * (p - yC));
+
+                Console.WriteLine("Area de X = " + areaX.ToString("F4", CI));
+                Console.WriteLine("Area de Y = " + areaY.ToString("F4", CI));
+
+                if (areaX > areaY)
+                {
+                    Console.WriteLine("Maior área : X");
+                }
+                else
+                {
+                    Console.WriteLine("Maior área : Y");
+                }
+
+            }
+        }
+    }
+}
